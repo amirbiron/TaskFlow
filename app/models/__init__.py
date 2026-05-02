231ee027e309f44ee7c6c91e77d4038d2fc8b78b
amir haim <amirbiron@gmail.com>
2026-05-02 17:38:12 +0300

    Initial import from ZIP via bot

---
+++ app/models/__init__.py
@@ -0,0 +1,16 @@
+from app.models.base import MongoBaseModel, PyObjectId
+from app.models.client import Client, ClientCreate, ClientUpdate, ClientWithStats
+from app.models.project import Project, ProjectCreate, ProjectUpdate, ProjectStatus
+from app.models.task import Task, TaskCreate, TaskUpdate, TaskStatus, TaskPriority
+from app.models.attachment import Attachment, AttachmentCreate
+from app.models.tag import Tag, TagCreate, TagUpdate
+
+__all__ = [
+    "MongoBaseModel",
+    "PyObjectId",
+    "Client", "ClientCreate", "ClientUpdate", "ClientWithStats",
+    "Project", "ProjectCreate", "ProjectUpdate", "ProjectStatus",
+    "Task", "TaskCreate", "TaskUpdate", "TaskStatus", "TaskPriority",
+    "Attachment", "AttachmentCreate",
+    "Tag", "TagCreate", "TagUpdate",
+]
